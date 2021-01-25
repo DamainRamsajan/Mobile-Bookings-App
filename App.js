504b7ab4375202503bbc854244b45d84fe554fdf
Damain Ramsajan <dpramsajan@gmail.com>
2021-01-25 21:14:21 +0100

    React native elements setup, fixed and node modules re installeed

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EnTypo from "react-native-vector-icons/EnTypo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!<Entypo name = {"500px"} /></Text>
       <StatusBar style="auto" />
     </View>
   );
